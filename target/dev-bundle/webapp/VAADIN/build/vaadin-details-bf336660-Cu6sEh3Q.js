9dec8a132a8ce9c6af0eeb98a5cc7540758b32c9
CODEsOwnDesign <gideon-yebei@proton.me>
2024-03-20 16:13:09 +0300

    remove .gitignore -> gitignore

---
+++ target/dev-bundle/webapp/VAADIN/build/vaadin-details-bf336660-Cu6sEh3Q.js
@@ -0,0 +1,1 @@
+import{S as e}from"./indexhtml-Bu9A2Xyg.js";const t={tagName:"vaadin-details",displayName:"Details",elements:[{selector:"vaadin-details",displayName:"Root element",properties:e}]};export{t as default};
---
+++ target/dev-bundle/webapp/VAADIN/build/vaadin-map-d40a0116-Bamn-1pe.js
@@ -0,0 +1,1 @@
+import{I as a}from"./indexhtml-Bu9A2Xyg.js";const o={tagName:"vaadin-map",displayName:"Map",elements:[{selector:"vaadin-map",displayName:"Root element",properties:[a.backgroundColor,a.borderColor,a.borderWidth,a.borderRadius,a.padding]}]};export{o as default};
